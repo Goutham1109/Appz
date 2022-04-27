d227843f85a50b29bd8a2d753a5fa919202a7688
goutham <gouthambe2016@gmail.com>
2022-04-27 23:07:59 +0530

    setup and component/service creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AlertModule } from 'ngx-bootstrap/alert';
+import { NavbarComponent } from './contacts/navbar/navbar.component';
+import { ContactManagerComponent } from './contacts/contact-manager/contact-manager.component';
+import { AddContactComponent } from './contacts/add-contact/add-contact.component';
+import { ViewContactComponent } from './contacts/view-contact/view-contact.component';
+import { EditContactComponent } from './contacts/edit-contact/edit-contact.component';
+import { SpinnerComponent } from './contacts/spinner/spinner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ContactManagerComponent,
+    AddContactComponent,
+    ViewContactComponent,
+    EditContactComponent,
+    SpinnerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AlertModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
